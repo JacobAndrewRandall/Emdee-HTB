19ab3dbdc32ae6d61a87fd2f9d0d9e9661b71f27
JacobAndrewRandall <74732661+JacobAndrewRandall@users.noreply.github.com>
2020-11-19 18:15:19 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+from hashlib import md5
+from bs4 import BeautifulSoup as BS
+import requests
+url = "http://143.110.168.246:30780"
+
+
+def encrypt_to_md5(string):
+    test = md5(string.encode())
+    return test.hexdigest()
+
+
+s = requests.Session()
+site = s.get(url)
+if site.status_code == 200:
+    soup = BS(site.text, features="html.parser")
+    phrase = soup.find('h3').get_text()
+    encrypted_phrase = encrypt_to_md5(phrase)
+    post_request = s.post(url, data={'hash': encrypted_phrase})
+    print(post_request.text)
+else:
+    print(f"Error getting to {url} Status code:{site.status_code}")
